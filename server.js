833969423bc2bd531b8716b2257c0d8a431d76ba
DIMANVAZ <DIMANVAZ04@gmail.com>
2021-02-14 02:28:07 +0300

    вставил кусÐок фетчка

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const { URLSearchParams } = require('url');
 const fs = require('fs');
 const params = new URLSearchParams();
 const text = 'Это первый раз, когда у нас получилось'
+
+params.append('text', text);
+params.append('voice', 'oksana');
+params.append('emotion', 'good');
+params.append('lang', 'ru-RU');
+params.append('speed', '1.0');
+params.append('format', 'oggopus');
 const port = process.env.PORT||3333;
 
     //подключаем статический  index.html:
@@ -19,9 +26,28 @@ const port = process.env.PORT||3333;
 
     //пробуем выполнять команды скрипта аж при заходе на страницу
     app.get('/directFetch', (req, res) => {
-        //alert('directFetchAlerting') //убрать
-        res.send('res.send is executed')
-        console.log('direct Fetch consoling')
+        //alert('directFetchAlerting') //убрать - вызывает ошибки
+        res.send('res.send is executed') //работает на хероку
+        console.log('direct Fetch consoling') //не работает на хероку
+
+  //----------------------пробуем впендюрить апи----------------------------------------
+        fetch('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', {
+            method: 'post',
+            body: params,
+            headers: {
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Api-Key ' + api_key,
+            },
+        })
+            .then(res => {
+                console.log(res);
+                // return res.json();
+                const dest = fs.createWriteStream(`./fio.ogg`);
+                res.body.pipe(dest);
+            })
+            .catch(err => console.error(err));
+        //----------------------конец апи----------------------------------------
+
     });
 
     app.listen(port);
